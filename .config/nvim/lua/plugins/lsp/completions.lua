9dcb65cea52a98dd2ddac1838a9d1d59fd88318a
Doni <vitringr@pm.me>
2025-06-22 18:21:31 +0300

    update(nvim): remove copilot

--- .config/nvim/lua/plugins/lsp/completions.lua
+++ .config/nvim/lua/plugins/lsp/completions.lua
@@ -36,7 +36,6 @@ return {
       }),
 
       sources = cmp.config.sources({
-        { name = "copilot", group_index = 2 },
         { name = "nvim_lsp", group_index = 2 },
         { name = "luasnip",  group_index = 2 },
         { name = "buffer",   group_index = 2 },
